2ff2bfcd493073400d81f97a4cd3caf3fad48d1c
Sergei Posysaev <43289846+Posysaev@users.noreply.github.com>
2019-02-11 18:23:09 +0200

    Update quick_check.py

--- quick_check.py
+++ quick_check.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # specify your path where to save your data
     path = 'E:\\work\\Results_aflow\\GitHub\\'
     # and desierd anion and cations
-    cation = 'Fe'
+    cation = 'Mn'
     anion = 'F'
     #if you already downloaded data after first launch, make it True
     data_saved = False
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     if not data_saved:
         db = get_links(cation, anion)
         #better to save links
-        db.to_csv(path+cation+anion+'_links'+'.csv')
+        db.to_csv(path+cation+anion+'_links'+'.csv', index=False)
         #uncomment if saved and comment upper one
-#        db = pd.read_csv(path+cation+anion+'_links'+'.csv')
+        db = pd.read_csv(path+cation+anion+'_links'+'.csv')
         #assign oxidation states based on the number of anions
         db['oxidation_state'] = db.apply(lambda row: oxidation_state(row, cation, anion), axis=1)
         #finds all digits in 'aurl' and takes only last one
@@ -28,7 +28,7 @@ if __name__ == '__main__':
         #drop records without Bader charge
         db = db.dropna(subset=['cation_charges']) 
         #save data
-        db.to_csv(path+cation+anion+'.csv')
+        db.to_csv(path+cation+anion+'.csv', index=False)
     
     else:
         db = pd.read_csv(path+cation+anion+'.csv')
@@ -38,7 +38,6 @@ if __name__ == '__main__':
     #converting database of compounds to database of atoms and drop columns
     db_for_each = to_db_with_bader_for_each_atom(db, atom_type='cation')
 
-    #plot
+    #plot    
     plot_os_vs_bader(db_for_each, cation, anion)
     
-
